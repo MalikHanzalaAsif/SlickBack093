d3023ef50cf442ef3ca7ea7265b33b66ed3e8ac8
Muhammad Hanzala <malikhanzalaasif@gmail.com>
2025-01-30 06:33:19 -0800

    made some changes in email sending route

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -38,9 +38,29 @@ const getOrderDetails = async (orderId, accessToken) => {
     }
 };
 
-
 export const sendEmails = async (formData, orderDetails, orderId) => {
     try {
+        const breakdown = orderDetails.purchase_units[0].amount.breakdown || {};
+
+        const sellerReceivableBreakdown = orderDetails.purchase_units[0].payments.captures[0].seller_receivable_breakdown || {};
+
+        const paypalFee = sellerReceivableBreakdown.paypal_fee ? `${sellerReceivableBreakdown.paypal_fee.value} ${sellerReceivableBreakdown.paypal_fee.currency_code}` : "N/A";
+
+        const exchangeRate = sellerReceivableBreakdown.exchange_rate ? `${sellerReceivableBreakdown.exchange_rate.target_currency} to ${sellerReceivableBreakdown.exchange_rate.source_currency} = ${sellerReceivableBreakdown.exchange_rate.value}` : "N/A";
+
+        const grossAmount = sellerReceivableBreakdown.gross_amount ? `${sellerReceivableBreakdown.gross_amount.value} ${sellerReceivableBreakdown.gross_amount.currency_code}` : "N/A";
+
+        const netAmount = sellerReceivableBreakdown.net_amount ? `${sellerReceivableBreakdown.net_amount.value} ${sellerReceivableBreakdown.net_amount.currency_code}` : "N/A";
+
+        const receivableAmount = sellerReceivableBreakdown.receivable_amount ? `${sellerReceivableBreakdown.receivable_amount.value} ${sellerReceivableBreakdown.receivable_amount.currency_code}` : "N/A";
+
+        const shipping = breakdown.shipping ? `${breakdown.shipping.value} ${breakdown.shipping.currency_code}` : "0.00";
+
+        const tax = breakdown.tax_total ? `${breakdown.tax_total.value} ${breakdown.tax_total.currency_code}` : "0.00";
+
+        const discount = breakdown.discount ? `${breakdown.discount}` : "0.00";
+
+
         const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
@@ -54,9 +74,10 @@ export const sendEmails = async (formData, orderDetails, orderId) => {
             return `
                 ITEM ${index + 1}:
                 - Name: ${item.name}
+                - Size: "Confirm from customer"
                 - Quantity: ${item.quantity}
-                - Price: $${Number(item.unit_amount.value).toFixed(2)}
-                - Total: $${(item.quantity * Number(item.unit_amount.value)).toFixed(2)}
+                - Price: ${Number(item.unit_amount.value).toFixed(2)} ${item.unit_amount.currency_code}
+                - Total: ${(item.quantity * Number(item.unit_amount.value)).toFixed(2)} ${item.unit_amount.currency_code}
             `;
         }).join("\n");
 
@@ -78,11 +99,21 @@ export const sendEmails = async (formData, orderDetails, orderId) => {
                 
                 -------- ORDER DETAILS --------
                 ORDER ID: ${orderId}
-                TOTAL: $${orderDetails.purchase_units[0].amount.value}
+                TOTAL: ${orderDetails.purchase_units[0].amount.value} ${orderDetails.purchase_units[0].amount.currency_code}
+                PAYPAL FEE: ${paypalFee}
+                SHIPPING: ${shipping} 
+                TAX: ${tax} 
+                DISCOUNT: ${discount}
+                EXCHNAGE RATE: ${exchangeRate}
+                GROSS AMOUNT: ${grossAmount}
+                NET AMOUNT: ${netAmount}
+                FINAL AMOUNT RECEIVED: ${receivableAmount}
+
+                
                 CART ITEMS:
                 ${cartItemsString}
                 
-                -------- PAYPAL ACCOUNT DETAILS --------
+                -------- CUSTOMER PAYPAL ACCOUNT DETAILS --------
                 ADDRESS: ${orderDetails.purchase_units[0].shipping.address.address_line_1 || "Not Provided"}
                 CITY: ${orderDetails.purchase_units[0].shipping.address.admin_area_2 || "Not Provided"}
                 STATE: ${orderDetails.purchase_units[0].shipping.address.admin_area_1 || "Not Provided"}
@@ -93,6 +124,7 @@ export const sendEmails = async (formData, orderDetails, orderId) => {
                 ORDER TIME: ${orderDetails.create_time}
             `,
         };
+
         const ownerInfo = await transporter.sendMail(ownerMailOptions);
         console.log("Email sent successfully to owner:", ownerInfo.response);
 
@@ -100,7 +132,7 @@ export const sendEmails = async (formData, orderDetails, orderId) => {
             from: process.env.FROM_EMAIL,
             to: formData.email,
             subject: "Order placed succesfully on SlickBack093",
-            text: `Dear ${user.name}/${formData.firstName} ${formData?.lastName}!
+            text: `Dear ${formData.firstName} ${formData?.lastName}!
              Thank you for your purchase. 
              Your order has been placed successfully.
              Your order ID is ${orderId}. 
@@ -110,7 +142,6 @@ export const sendEmails = async (formData, orderDetails, orderId) => {
         // Send email asynchronously
         const userInfo = await transporter.sendMail(userMailOptions);
         console.log("Email sent successfully to user:", userInfo.response);
-
     } catch (error) {
         console.error("Failed to send emails:", error);
         throw error;
