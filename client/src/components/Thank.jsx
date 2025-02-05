79761bd517294fd42add978a4e9495b32aef67ad
Muhammad Hanzala <malikhanzalaasif@gmail.com>
2025-02-05 12:55:38 -0800

    changed thankyou page animation

--- client/src/components/Thank.jsx
+++ client/src/components/Thank.jsx
@@ -4,8 +4,8 @@ import "../styles/Thank.css";
 const Thank = () => {
     return (
         <div id="thank" className='h-screen flex flex-col items-center justify-center'>
-            <video autoPlay loop muted preload="auto" className="loader-animation h-60">
-                <source src="/order-placed.webm" type="video/webm" />
+            <video autoPlay loop muted preload="auto" className="loader-animation h-80">
+                <source src="/order-placed.webm" type="video/webm" alt="order placed"/>
                 Loading...
             </video>
             <h1 className='text-4xl p-3 rounded-lg text-white mb-8' id='thankHeading'>Your Order Has been Placed!</h1>
