bb72fc0c7dc8c9e95841d74a53b8779ba2c47ab1
Muhammad Hanzala <malikhanzalaasif@gmail.com>
2025-03-18 15:23:56 -0700

    changed marquee text

--- client/src/Marquee.jsx
+++ client/src/Marquee.jsx
@@ -4,10 +4,10 @@ export default function Marquee() {
   return (
     <div className="marquee-box flex-1">
     <div className="marquee-content">
-      <span>Welcome to the Brand Name!</span>
+      <span>Welcome to the Slick back!</span>
       <span>Stay tuned for updates!</span>
       <span>Enjoy the 45% off in winter sale!</span>
-      <span>Welcome to the Brand Name!</span>
+      <span>Welcome to the Slick Back!</span>
     </div>
   </div>
   );
